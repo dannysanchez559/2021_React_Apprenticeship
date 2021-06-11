6e9a3119129178c7d80071b39262a7d8e8de17b3
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2021-06-10 20:11:27 -0700

    refactor(api): change Api to use FACC pattern

--- movie-app/src/utils/api.js
+++ movie-app/src/utils/api.js
@@ -1,23 +1,52 @@
-export const getMoviesByName = async (name) => {
-  try {
-    const url = `${process.env.REACT_APP_OMDB_API_URL}/?t=${name}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+import { Component } from 'react';
 
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => data);
-  } catch (error) {
-    console.error(error);
+export class Api extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+    };
+
+    this.getMoviesByName = this.getMoviesByName.bind(this);
+    this.getMovieDetailsById = this.getMovieDetailsById.bind(this);
   }
-};
 
-export const getMovieDetailsById = async (id) => {
-  try {
-    const url = `${process.env.REACT_APP_OMDB_API_URL}/?i=${id}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+  getMovieDetailsById = async () => {
+    if (this.props.searchTerm) {
+      try {
+        const url = `${process.env.REACT_APP_OMDB_API_URL}/?i=${this.props.searchTerm}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => data);
-  } catch (error) {
-    console.error(error);
+        await fetch(url)
+          .then((response) => response.json())
+          .then((data) => this.setState({ movie: data }));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  getMoviesByName = async () => {
+    if (this.props.searchTerm){
+      try {
+        const url = `${process.env.REACT_APP_OMDB_API_URL}/?t=${this.props.searchTerm}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+
+        await fetch(url)
+          .then((response) => response.json())
+          .then((data) => this.setState({ movie: data }));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  render(){
+    return(
+      this.props.children({
+        getMovieDetailsById: this.getMovieDetailsById,
+        getMoviesByName: this.getMoviesByName,
+        movie: this.state.movie,
+      })
+    );
   }
-};
+
+}
