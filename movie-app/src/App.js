945087b8fe2fb5e85bd7577e50ca986cf95fb527
Jay Dampitan <jaykenethpd@gmail.com>
2021-06-14 11:46:30 -0700

    feat(movieapp): creates movie card component, and render movie card component on to app.js

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,27 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { getMoviesByName, getMovieDetailsById } from "./utils/api";
+import MovieCard from "./components/MovieCard";
 
 function App() {
-  getMoviesByName("Guardians of the Galaxy Vol. 2");
+  const moviesByName = getMoviesByName("Guardians of the Galaxy Vol. 2");
   getMovieDetailsById("tt0091541");
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {moviesByName && 
+        <MovieCard 
+          title={moviesByName.Title} 
+          posterUrl={moviesByName.Poster}
+          type={moviesByName.Type}
+        />}
     </div>
   );
 }
